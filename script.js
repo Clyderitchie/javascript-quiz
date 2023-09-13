86d30b12d0f347914dcc90879cbe471595db13cc
francis <clyderitchie@yahoo.com>
2023-09-12 22:14:05 -0700

    debugged some code with the questions showing and the timer

--- script.js
+++ script.js
@@ -43,38 +43,39 @@ var intervalId;
 var score = 0;
 
 // Render currentQuestionIndex once the user clicks the start button
-startBtn.addEventListener('click', function() {
+startBtn.addEventListener('click', function () {
     showQuestion();
     timer();
     displayEvent();
-    
+
 });
 
 // Timer function
-function timer () {
+function timer() {
     //starting the timer
-    intervalId = setInterval(function() {
+    intervalId = setInterval(function () {
         timeleft--;
         timeEl.textContent = timeleft;
-    }, 1000);
-    
-    if(timeleft === 0) {
-        clearInterval(intervalId);
-    }     
+        if (timeleft <= 0) {
+            clearInterval(intervalId);
+        }
+    }, 10);
+
+   
 }
 
 // function to show current question and choices for that question
-function showQuestion () {
+function showQuestion() {
     questionContainerEl.textContent = questions[currentQuestionIndex].title;
     for (let i = 0; i < questions[currentQuestionIndex].choices.length; i++) {
         var choiceBtn = document.createElement('button');
         choiceBtn.textContent = questions[currentQuestionIndex].choices[i];
         questionContainerEl.append(choiceBtn);
-        choiceBtn.addEventListener('click', function(event) {
-            if (event.target.textContent === questions[currentQuestionIndex].answer){
+        choiceBtn.addEventListener('click', function (event) {
+            if (event.target.textContent === questions[currentQuestionIndex].answer) {
                 console.log('correct');
                 score++;
-            } else if (event.target.textContent != questions[currentQuestionIndex].answer){
+            } else if (event.target.textContent != questions[currentQuestionIndex].answer) {
                 timeleft -= 15;
                 console.log('wrong answer');
             }
@@ -86,11 +87,21 @@ function showQuestion () {
 
 // Sets displays to block or none depending on users actions
 function displayEvent() {
-    if (questionContainerEl.style.display === 'none' && startScreenEl.style.display === 'block') {
-        questionContainerEl.style.display = 'block';
-        startScreenEl.style.display = 'none';
+    // if (questionContainerEl.style.display === 'none' && startScreenEl.style.display === 'block') {
+    //     questionContainerEl.style.display = 'block';
+    //     startScreenEl.style.display = 'none';
+    // } else {
+    //     startScreenEl.style.display = 'block';
+    //     questionContainerEl.style.display = 'none';
+    // }
+
+    if (questionContainerEl.classList[0] === 'hidden') {
+        questionContainerEl.classList.remove('hidden');
+        startScreenEl.classList.add('hidden');
     } else {
-        startScreenEl.style.display = 'block';
-        questionContainerEl.style.display = 'none';
+        questionContainerEl.classList.add('hidden');
+        startScreenEl.classList.remove('hidden');
     }
+
+    console.log(questionContainerEl.classList);
 }
